becced282bd14cc1d2df67285472212cec2c4046
ppaudel93 <ppaudel93@gmail.com>
2018-07-27 13:30:29 +0545

    added user registration partially added user login changed from anko to xml rounded corners of dialogboxes

--- app/src/main/java/com/example/ppaud/bhoodie_new2/MainActivity.kt
+++ app/src/main/java/com/example/ppaud/bhoodie_new2/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.ppaud.bhoodie_new2
 
+import android.annotation.SuppressLint
+import android.app.Dialog
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.graphics.drawable.GradientDrawable
+import android.hardware.input.InputManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AlertDialog
+import android.text.InputType
+import android.text.InputType.TYPE_TEXT_VARIATION_PASSWORD
+import android.util.Log
 import android.view.Gravity
+import android.view.View
+import android.widget.LinearLayout
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.registerdialog.*
+import kotlinx.android.synthetic.main.registerdialog.view.*
 import org.jetbrains.anko.*
 
 class MainActivity : AppCompatActivity() {
@@ -28,137 +44,75 @@ class MainActivity : AppCompatActivity() {
         //setColor(Color.parseColor("#474747"))
         setStroke(5, Color.parseColor("#d06666"))
     }
+    private val RC_SIGN_IN = 123
+    var mAuth = FirebaseAuth.getInstance()!!
+    var user: FirebaseUser? = null
+    fun showSnackbar(id: Int,layout: LinearLayout){
+        Snackbar.make(layout,resources.getString(id),Snackbar.LENGTH_LONG).show()
+    }
 
-    //private var serverconnecturl=""
+    override fun onStart() {
+        super.onStart()
+//        val currentuser: FirebaseUser = mAuth.currentUser!!
 
+    }
+
+    @SuppressLint("ResourceType")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        verticalLayout {
-            padding = dip(30)
-            editText {
-                hint = "Email"
-                textSize = 20f
-                gravity= Gravity.CENTER
-                background=shape_roundedrect()
-            }.lparams() { topPadding=dip(50)
-                width= matchParent
-                height=dip(60)
-                topMargin=dip(20)
-                leftMargin=dip(20)
-                rightMargin=dip(20)
-            }
-            editText {
-                hint = "Password"
-                textSize = 20f
-                gravity= Gravity.CENTER
-                background=shape_roundedrect()
-            }.lparams() {
-                width= matchParent
-                height=dip(60)
-                topMargin=dip(20)
-                leftMargin=dip(20)
-                rightMargin=dip(20)
-            }
-            var loginbutton = button {
-                text = getString(R.string.login)
-                textSize=20f
-                allCaps=false
-                textColor=getColor(R.color.textprimary)
-                gravity= Gravity.CENTER
-                background=shape_roundedrectbut()
-                setOnClickListener{
-                    startActivity<MapsActivity>()
-                }
-            }.lparams(){
-                width = dip(120)
-                height = dip(60)
-                topMargin=dip(20)
-                //leftMargin=dip(120)
-            }
-            var registerbutton =  button{
-                text=getString(R.string.register)
-                textSize=20f
-                allCaps=false
-                gravity= Gravity.CENTER
-                textColor=getColor(R.color.textprimary)
-                background=shape_roundedrectbut()
-                setOnClickListener(){
-                    alert(){
-                        customView() {
-                            width= wrapContent
-                            height= wrapContent
-                            verticalLayout {
-                                background= shape_roundeddialog()
-                                bottomPadding=dip(20)
-                                editText {
-                                    hint = "Full Name"
-                                    textSize = 20f
-                                    gravity = Gravity.CENTER
-                                    background = shape_roundedrect()
-                                }.lparams() {
-                                    topPadding = dip(50)
-                                    width = matchParent
-                                    height = dip(60)
-                                    topMargin = dip(20)
-                                    leftMargin = dip(20)
-                                    rightMargin = dip(20)
-                                }
-                                editText {
-                                    hint = "Email"
-                                    textSize = 20f
-                                    gravity = Gravity.CENTER
-                                    background = shape_roundedrect()
-                                }.lparams() {
-                                    topPadding = dip(50)
-                                    width = matchParent
-                                    height = dip(60)
-                                    topMargin = dip(20)
-                                    leftMargin = dip(20)
-                                    rightMargin = dip(20)
-                                }
-                                editText {
-                                    hint = "Password"
-                                    textSize = 20f
-                                    gravity = Gravity.CENTER
-                                    background = shape_roundedrect()
-                                }.lparams() {
-                                    topPadding = dip(50)
-                                    width = matchParent
-                                    height = dip(60)
-                                    topMargin = dip(20)
-                                    leftMargin = dip(20)
-                                    rightMargin = dip(20)
-                                }
-                                button {
-                                    text = getString(R.string.register)
-                                    textSize = 20f
-                                    allCaps = false
-                                    gravity = Gravity.CENTER
-                                    background = shape_roundedrectbut()
-                                    setOnClickListener {
-                                        //startActivity<MapsActivity>()
-                                    }
-                                }.lparams() {
-                                    width = dip(120)
-                                    height = dip(60)
-                                    topMargin = dip(20)
-                                    horizontalGravity = Gravity.CENTER
-                                    //leftMargin=dip(120)
-                                }
+        val view = View.inflate(this,R.layout.registerdialog,null)
+        emaillogin.background=shape_roundedrect()
+        passwordlogin.background=shape_roundedrect()
+        loginbutton.background=shape_roundedrectbut()
+        registerbutton.background=shape_roundedrectbut()
 
+
+        //val loginbut = findViewById(R.id.emaillogin)
+
+        mAuth=FirebaseAuth.getInstance()
+        loginbutton.setOnClickListener {
+            mAuth.signInWithEmailAndPassword(emaillogin.text.toString(),passwordlogin.text.toString())
+                            .addOnCompleteListener{
+                                if(it.isSuccessful){
+                                    Log.i("user_login","Logged In Successfully")
+                                    user= mAuth.currentUser!!
+
+                                }
+                                else{
+                                    Log.i("user_login","Login Failed")
+
+                                }
                             }
-                        }
-                        overridePendingTransition(R.anim.abc_popup_enter,R.anim.abc_popup_exit)
-                    }.show()
-                    //overridePendingTransition(R.anim.abc_popup_enter,R.anim.abc_popup_exit)
-                }
-            }.lparams(){
-                width = dip(120)
-                height = dip(60)
-                topMargin=dip(20)
-                horizontalGravity= Gravity.CENTER
-                //leftMargin=dip(120)
+                    //startActivity<MapsActivity>()
+        }
+        registerbutton.setOnClickListener {
+            val builder = AlertDialog.Builder(this@MainActivity)
+            builder.setView(view)
+            val dialog: AlertDialog = builder.create()
+            val newdialog: Dialog ?= null
+            dialog.show()
+            dialog.window.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+            view.alertbuttonregister.background=shape_roundedrectbut()
+//            view.dialogname.background=shape_roundedrect()
+//            view.registeremail.background=shape_roundedrect()
+//            view.registerpass.background=shape_roundedrect()
+            //alertbuttonregister.background=shape_roundedrectbut()
+
+            view.alertbuttonregister.setOnClickListener {
+                mAuth.createUserWithEmailAndPassword(view.registeremail.text.toString(),view.registerpass.text.toString())
+                                                .addOnCompleteListener {
+                                                    if (it.isSuccessful){
+                                                        Log.i("task_successful","New User created")
+                                                        Toast.makeText(this@MainActivity,"New User Created",Toast.LENGTH_LONG).show()
+                                                        user = mAuth.currentUser
+                                                        dialog.dismiss()
+                                                    }
+                                                    else{
+                                                        Log.i("task_failed","User Creation falied")
+                                                    }
+                                                }
+                                        //startActivity<MapsActivity>()
             }
 
         }
