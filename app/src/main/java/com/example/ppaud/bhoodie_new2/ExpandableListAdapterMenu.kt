89024349baec0e0dabea5bd557dd3ad2b3c4e1bd
ppaudel93 <ppaudel93@gmail.com>
2018-07-29 18:13:06 +0545

    29-07-2018 location details updated extendable lists added dynamic photo viewer added populated location details more handled some UI design

---
+++ app/src/main/java/com/example/ppaud/bhoodie_new2/ExpandableListAdapterMenu.kt
@@ -0,0 +1,87 @@
+package com.example.ppaud.bhoodie_new2
+
+import android.content.Context
+import android.media.Image
+import android.text.Layout
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.*
+import org.w3c.dom.Text
+import com.example.ppaud.bhoodie_new2.PlaceInfo.Menus
+
+class ExpandableListAdapterMenu(var context: Context,var expandableListView: ExpandableListView,var header: MutableList<String>,var tail: MutableList<MutableList<Menus>>): BaseExpandableListAdapter(){
+    override fun getGroup(groupPosition: Int): String {
+        return header[groupPosition]
+    }
+
+    override fun isChildSelectable(groupPosition: Int, childPosition: Int): Boolean {
+        return true
+    }
+
+    override fun hasStableIds(): Boolean {
+        return false
+    }
+
+    override fun getGroupView(groupPosition: Int, isExpanded: Boolean, convertView: View?, parent: ViewGroup?): View? {
+        var convertView = convertView
+        if (convertView == null){
+            val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+            convertView = inflater.inflate(R.layout.layout_group,null)
+
+        }
+        val title = convertView?.findViewById<TextView>(R.id.listdays)
+        title?.text=getGroup(groupPosition)
+        title?.setOnClickListener {
+            if(expandableListView.isGroupExpanded(groupPosition))
+            {expandableListView.collapseGroup(groupPosition)
+            }
+            else
+            {expandableListView.expandGroup(groupPosition)
+
+            }
+            Toast.makeText(context, getGroup(groupPosition),Toast.LENGTH_SHORT).show()
+        }
+        return convertView
+    }
+
+    override fun getChildrenCount(groupPosition: Int): Int {
+        return tail[groupPosition].size
+    }
+
+    override fun getChild(groupPosition: Int, childPosition: Int): Menus {
+        return tail[groupPosition][childPosition]
+    }
+
+    override fun getGroupId(groupPosition: Int): Long {
+        return groupPosition.toLong()
+    }
+
+    override fun getChildView(groupPosition: Int, childPosition: Int, isLastChild: Boolean, convertView: View?, parent: ViewGroup?): View? {
+        var convertView = convertView
+        if (convertView == null){
+            val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+            convertView = inflater.inflate(R.layout.childofmenu,null)
+
+        }
+        val title = convertView?.findViewById<TextView>(R.id.listitem)
+        val price = convertView?.findViewById<TextView>(R.id.listprice)
+        val thumbsup = convertView?.findViewById<ImageView>(R.id.thumbsup)
+        val thumbsdown = convertView?.findViewById<ImageView>(R.id.thumbsdown)
+        title?.text= getChild(groupPosition,childPosition).item
+        price?.text=getChild(groupPosition,childPosition).price.toString()
+        title?.setOnClickListener {
+            Toast.makeText(context, getChild(groupPosition,childPosition).toString(),Toast.LENGTH_SHORT).show()
+        }
+        return convertView
+    }
+
+    override fun getChildId(groupPosition: Int, childPosition: Int): Long {
+        return childPosition.toLong()
+    }
+
+    override fun getGroupCount(): Int {
+        return header.size
+    }
+
+}
--- app/src/main/java/com/example/ppaud/bhoodie_new2/PlaceInfo.kt
+++ app/src/main/java/com/example/ppaud/bhoodie_new2/PlaceInfo.kt
@@ -1,74 +1,227 @@
 package com.example.ppaud.bhoodie_new2
 
+import android.content.pm.PackageManager
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.graphics.drawable.Drawable
 import android.media.Image
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import android.support.v7.app.AlertDialog
 import android.util.Log
-import android.widget.ImageView
+import android.view.View
+import android.widget.*
 import com.bumptech.glide.Glide
 import com.google.gson.GsonBuilder
 import kotlinx.android.synthetic.main.activity_place_info.*
 import okhttp3.*
+import org.jetbrains.anko.*
+import org.w3c.dom.Text
 import java.io.IOException
 
 class PlaceInfo : AppCompatActivity() {
     private var defaulturl: String = "https://bhoodie.herokuapp.com"
     val temp: MutableList<String> = ArrayList()
+    val temp2: MutableList<Menus> = ArrayList()
     val header: MutableList<String> = ArrayList()
     val tail: MutableList<MutableList<String>> = ArrayList()
+    val header2: MutableList<String> = ArrayList()
+    val tail2: MutableList<MutableList<Menus>> = ArrayList()
+    val photourl: MutableList<String> = ArrayList()
+    private val LOCATION_REQUEST_CODE=101
+    private var imageurlcount: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_place_info)
         var placeid = intent.getStringExtra("placeid")
-        Handler().postDelayed({
-            header.add("Opening Hours")
-            tail.add(temp)
-            val title: String = "Opening Hours"
-            expandablelist.setAdapter(ExpandableListAdapter(this@PlaceInfo,expandablelist,header,tail))
-            Log.i("listcreated","LIST HAS BEEN CREATED")
-        },5000)
+        var rating = intent.getStringExtra("rating")
+        val isitopen = intent.getBooleanExtra("openornot",false)
+        val ratingtext = findViewById<TextView>(R.id.rating)
+        ratingtext.text=rating
+        getplaceinfo(placeid)
+        val Openornot =findViewById<TextView>(R.id.istheplaceopen)
+        val openicon = findViewById<ImageView>(R.id.opencloseicon)
+        if (isitopen==true){
+            openicon.backgroundResource=R.drawable.openicon
+            Openornot.text=getText(R.string.open)
+        }
+        else{
+            openicon.backgroundResource=R.drawable.closedicon
+            Openornot.text=getText(R.string.closed)
+        }
+        val previous = findViewById<ImageButton>(R.id.previousimage)
+        val next = findViewById<ImageButton>(R.id.nextimage)
+        val backbutton = findViewById<Button>(R.id.placeinfoback)
+        backbutton.setOnClickListener {
+            finish()
+        }
+        previous.backgroundColor = Color.TRANSPARENT
+        next.backgroundColor=Color.TRANSPARENT
 
         Log.i("placeid","the placeis is $placeid")
         //placeinfoname.setText()
        // val imageview = findViewById<ImageView>(R.id.placeimage)
         //Glide.with(this).load("https://lh3.googleusercontent.com/p/AF1QipOKmMiqTa5IuKLoF7C1aAJfoD5I5bGFxfPyedJj=s1600-w600").into(imageview)
-        getplaceinfo(placeid)
     }
 
     fun getplaceinfo(placeid: String){
-        val request = Request.Builder().url(defaulturl+"/api/pdetail/id=$placeid").build()
-        val client = OkHttpClient()
-        client.newCall(request).enqueue(object: Callback{
-            override fun onFailure(call: Call?, e: IOException?) {
-                Log.i("requeststatus","Request Failed")
+        doAsync {
+            val request = Request.Builder().url(defaulturl+"/api/pdetail/id=$placeid").build()
+            val client = OkHttpClient()
+            client.newCall(request).enqueue(object: Callback{
+                override fun onFailure(call: Call?, e: IOException?) {
+                    Log.i("requeststatus","Request Failed")
 
-            }
-
-            override fun onResponse(call: Call?, response: Response?) {
-
-                Log.i("requeststatus","Request Success")
-                val body = response?.body()?.string()
-                val gson = GsonBuilder().create()
-                val Mainobject = gson.fromJson(body,mainobject::class.java)
-                placeinfoname.text=Mainobject.name
-                for(item in Mainobject.opening_hours){
-                    temp.add(item)
                 }
-                placename.text=Mainobject.name
-                placelocation.text=Mainobject.address
-                placephoneno.text=Mainobject.formatted_phone_number
 
+                override fun onResponse(call: Call?, response: Response?) {
+
+                    Log.i("requeststatus","Request Success")
+                    val body = response?.body()?.string()
+                    val gson = GsonBuilder().create()
+                    val Mainobject = gson.fromJson(body,mainobject::class.java)
+                    placeinfoname.text=Mainobject.name
+                    val fooditems: MutableList<Menus> = ArrayList()
+                    for (item in Mainobject.photos){
+                        photourl.add(item)
+                    }
+                    for(item in Mainobject.opening_hours){
+                        temp.add(item)
+                    }
+                    for(item in Mainobject.menu){
+                        fooditems.add(item)
+                    }
+                    for(item in fooditems){
+                        temp2.add(item)
+                    }
+
+
+            uiThread {
+                val nameofplace = findViewById<TextView>(R.id.placename)
+                val locationofplace = findViewById<TextView>(R.id.placelocation)
+                val phoneofplace = findViewById<TextView>(R.id.placephoneno)
+                nameofplace.text=Mainobject.name
+                locationofplace.text=Mainobject.address
+                //if (Mainobject.formatted_phone_number!=null)
+                phoneofplace.text=Mainobject.formatted_phone_number
+                phoneofplace.isClickable=true
+                phoneofplace.setOnClickListener {
+                    val permission = ContextCompat.checkSelfPermission(this@PlaceInfo,android.Manifest.permission.CALL_PHONE)
+                    if (permission == PackageManager.PERMISSION_GRANTED){
+                        makeCall(Mainobject.formatted_phone_number)
+                    } else{
+                        requestPermission(android.Manifest.permission.CALL_PHONE,LOCATION_REQUEST_CODE)
+                    }
+
+                }
+                header.add("Opening Hours")
+                tail.add(temp)
+                header2.add("Menu")
+                tail2.add(temp2)
                 val imageview = findViewById<ImageView>(R.id.placeimage)
-                //Glide.with(this@PlaceInfo).load(Mainobject.photos[0]).into(imageview)
-            }
 
-        })
+                expandablelist.setAdapter(ExpandableListAdapter(this@PlaceInfo,expandablelist,header,tail))
+                menuexpandable.setAdapter(ExpandableListAdapterMenu(this@PlaceInfo,menuexpandable,header2,tail2))
+                expandablelist.setOnGroupExpandListener {
+                    expandablelist.layoutParams.height=dip(340)
+                }
+                expandablelist.setOnGroupCollapseListener {
+                    expandablelist.layoutParams.height= wrapContent
+                }
+                menuexpandable.setOnGroupExpandListener {
+                    menuexpandable.layoutParams.height= dip(200)
+                }
+                menuexpandable.setOnGroupCollapseListener {
+                    menuexpandable.layoutParams.height= wrapContent
+                }
+
+                Glide.with(this@PlaceInfo).load(photourl[imageurlcount]).into(imageview)
+                imageview.layoutParams.height= wrapContent
+                val nextbutton = findViewById<ImageButton>(R.id.nextimage)
+                val previousbutton = findViewById<ImageButton>(R.id.previousimage)
+                nextbutton.setOnClickListener {
+                    imageurlcount += 1
+                    if (imageurlcount==photourl.size) {
+                       imageurlcount=0
+                    }
+                    Glide.with(this@PlaceInfo).load(photourl[imageurlcount]).into(imageview)
+                    imageview.layoutParams.height= wrapContent
+
+                }
+                previousbutton.setOnClickListener{
+                    imageurlcount -= 1
+                    if (imageurlcount==-1){
+                        imageurlcount=photourl.size-1
+                    }
+                    Glide.with(this@PlaceInfo).load(photourl[imageurlcount]).into(imageview)
+                    imageview.layoutParams.height= wrapContent
+                }
+
+                val bikebutton = findViewById<ImageView>(R.id.parkingbikecheck)
+                val carbutton = findViewById<ImageView>(R.id.parkingcarcheck)
+                val smokingbutton = findViewById<ImageView>(R.id.smokingcheck)
+                val vatbutton = findViewById<ImageView>(R.id.vatcheck)
+                val scbutton = findViewById<ImageView>(R.id.servicetaxcheck)
+                val view = View.inflate(this@PlaceInfo,R.layout.moreplaceinfodialog,null)
+                val view2=View.inflate(this@PlaceInfo,R.layout.menuitemadddialog,null)
+                val builder = AlertDialog.Builder(this@PlaceInfo)
+                val builder2 = AlertDialog.Builder(this@PlaceInfo)
+                val addfoodbutton = findViewById<ImageView>(R.id.addfooditem)
+                builder.setView(view)
+                builder2.setView(view2)
+                val dialog: AlertDialog = builder.create()
+                val dialog2: AlertDialog = builder2.create()
+                bikebutton.isClickable=true
+                carbutton.isClickable=true
+                smokingbutton.isClickable=true
+                vatbutton.isClickable=true
+                scbutton.isClickable=true
+                addfoodbutton.isClickable=true
+
+                bikebutton.setOnClickListener{
+                    dialog.show()
+                    dialog.window.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+                }
+                carbutton.setOnClickListener {
+                    dialog.show()
+                    dialog.window.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+                }
+                smokingbutton.setOnClickListener {
+                    dialog.show()
+                    dialog.window.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+                }
+                vatbutton.setOnClickListener {
+                    dialog.show()
+                    dialog.window.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+                }
+                scbutton.setOnClickListener {
+                    dialog.show()
+                    dialog.window.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+                }
+                addfoodbutton.setOnClickListener {
+                    dialog2.show()
+                    dialog2.window.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+                }
+
+            }
+        }
+            })
+
+        }
 
     }
 
-    class mainobject(val name: String,val address: String,val formatted_phone_number: String,val opening_hours: List<String>,val photos: List<String>,val review: List<Review>)
+    private fun requestPermission(permissionType: String,requestCode: Int){
+        ActivityCompat.requestPermissions(this, arrayOf(permissionType),requestCode)
+    }
+
+    class mainobject(val name: String,val address: String,val formatted_phone_number: String,val opening_hours: List<String>,val photos: List<String>,val review: List<Review>,val website: String, val menu: List<Menus>)
+
+    class Menus(val item: String,val price: Int,val votes: Int)
 
     class Review(val auth1: String)
 }
